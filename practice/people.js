a474d6c7144aab15ee35195375c96896db8e67f8
preethisara01 <sarathapreethi373@gmail.com>
2025-07-21 16:11:06 +0530

    #commit

---
+++ practice/people.js
@@ -0,0 +1,11 @@
+const person = ['sara','dhara','poorni','vishnu']
+const ages = [21,30,27,40];
+
+//console.log(person);
+console.log("it is the program")
+
+//mobile.exports = person;
+mobile.exports ={
+    persons,
+    ages,
+}
